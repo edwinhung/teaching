3abcf2561caae9568fbf344f994213575319efed
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-18 10:12:46 -0700

    make new Promise

---
+++ app.js
@@ -0,0 +1,16 @@
+const tennisPro = {
+  first: "Novak",
+  last: "Djokovic",
+  nick: "Nole",
+  getName() {
+    console.log(`${this.first} ${this.last}`);
+  },
+};
+
+tennisPro.getName();
+
+const tennisPro2 = {
+  first: "Rafael",
+  last: "Nadal",
+  nick: "Rafa",
+};
